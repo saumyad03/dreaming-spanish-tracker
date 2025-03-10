a8b1697619f057f6f879b5668ec90f5375bccb77
Saumya Dwivedi <saumyad03@gmail.com>
2025-03-09 20:27:44 -0400

    Dreaming Spanish external time adding logic, Spotify authentication logic

--- README.md
+++ README.md
@@ -1,1 +1,8 @@
 # dreaming-spanish-tracker
+This script integrates Dreaming Spanish's comprehensible input tracking capabilities with Spotify using Spotify's API
+## Setup
+1. Log in to Dreaming Spanish, go to developer tools, and store the authorization token from local storage in .env file as DS_AUTHORIZATION_TOKEN
+2. Log in to https://developer.spotify.com/dashboard, create an application, and store the application's Client ID and Client Secret as SPOTIFY_CLIENT_ID and SPOTIFY_CLIENT_SECRET in .env file
+3. Visit the following URL in your browser, replacing YOUR_CLIENT_ID with your Spotify Client ID, the redirect URI with whatever you set the redirect URI to when creating the Spotify application, and YOUR_RANDOM_STATE with a random string
+    - https://accounts.spotify.com/authorize?response_type=code&client_id=YOUR_CLIENT_ID&scope=user-read-recently-played&redirect_uri=http://localhost:8888/callback&state=YOUR_RANDOM_STATE
+4. After being redirected to the redirect URI, extract the authorization code from the code query parameter, and store it in .env file as SPOTIFY_AUTHORIZATION_CODE
---
+++ main.py
@@ -0,0 +1,77 @@
+from datetime import date
+import os
+from dotenv import load_dotenv
+import requests
+import base64
+load_dotenv()
+spotify_client_id = os.getenv("SPOTIFY_CLIENT_ID")
+spotify_client_secret = os.getenv("SPOTIFY_CLIENT_SECRET")
+authorization_code = os.getenv("SPOTIFY_AUTHORIZATION_CODE")
+redirect_uri = "http://localhost:8888/callback"
+token_url = 'https://accounts.spotify.com/api/token'
+data = {
+    'grant_type': 'authorization_code',
+    'code': authorization_code,
+    'redirect_uri': redirect_uri
+}
+auth = (spotify_client_id, spotify_client_secret)
+response = requests.post(token_url, data=data, auth=auth)
+access_token = response.json().get('access_token')
+refresh_token = response.json().get('refresh_token')
+if response.status_code == 200:
+    print('Access Token:', access_token)
+    print('Refresh Token:', refresh_token)
+else:
+    print(f"Error: {response.status_code}, {response.text}")
+data = {
+    'grant_type': 'refresh_token',
+    'refresh_token': refresh_token
+}
+credentials = f'{spotify_client_id}:{spotify_client_secret}'
+encoded_credentials = base64.b64encode(credentials.encode('utf-8')).decode('utf-8')
+headers = {
+    'Authorization': f'Basic {encoded_credentials}'
+}
+response = requests.post(token_url, data=data, headers=headers)
+new_access_token = response.json().get('access_token')
+if response.status_code == 200:
+    print("New access token:", new_access_token)
+else:
+    print("Failed to get new access token:", response.json())
+
+
+# response = requests.get('https://api.spotify.com/v1/me/player', headers={
+#     'Authorization': f'Bearer {access_token}'
+# })
+# print(response.status_code)
+# print(response.json())
+
+url = 'https://www.dreamingspanish.com/.netlify/functions/externalTime'
+payload = {
+    'date': str(date.today()),
+    'description': '',
+    'id': '',
+    'timeSeconds': 0,
+    'type': 'listening'
+}
+headers = {
+    'Authorization': f'Bearer {os.getenv("DS_AUTHORIZATION_TOKEN")}',
+}
+
+response = requests.post(url, json=payload, headers=headers)
+
+print("Status Code:", response.status_code)
+print("Response:", response.json())
+# Sample
+
+# date:"2025-03-08"
+# description:"example"
+# id:"1741426340694e9993dcb40f2"
+# timeSeconds: 240
+# type:"watching"
+
+# date: "2025-03-08"
+# description: "1"
+# id: "174142742422831511d252edeb"
+# timeSeconds:60
+# type:"listening"
